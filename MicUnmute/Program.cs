4a9bac1f5dd148d8030e4bcd99b8594513796bd5
fulfordcs <fulfordcs@gmail.com>
2020-10-22 17:01:43 +0100

    Add project files.

---
+++ MicUnmute/Program.cs
@@ -0,0 +1,43 @@
+﻿using AudioSwitcher;
+using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicUnmute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Microphone un-muter by P Dring");
+            Console.WriteLine("This program will detect and unmute all microphones / webcams");
+            Console.WriteLine("Detecting microphones...");
+            CoreAudioController controller = new CoreAudioController();
+            foreach (CoreAudioDevice device in controller.GetCaptureDevices())
+            {
+                Console.Write("  > Detected: " + device.FullName + ": ");
+                if(device.IsMuted)
+                {
+                    Console.Write("Muted - attempting to unmute");
+                    device.Mute(false);
+                    if(device.IsMuted)
+                    {
+                        Console.Write(" failed :(");
+                    } else
+                    {
+                        Console.Write(" success :)");
+                    }
+                } else
+                {
+                    Console.Write("Unmuted");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Done - press enter to close this window");
+            Console.ReadLine();
+        }
+    }
+}
